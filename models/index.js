55c1219d40e31641ef40e060d87de8d0d9c99297
Deadpool-223 <128416241+Deadpool-223@users.noreply.github.com>
2025-04-03 09:18:38 +0000

    fix Activity and location model

--- models/index.js
+++ models/index.js
@@ -31,6 +31,8 @@ db.Op = Op;
 
 db.user = require("./user.model.js")(sequelize, Sequelize);
 db.friendList = require("./friendList.model.js")(sequelize, Sequelize);
+db.location = require("./location.model.js")(sequelize, Sequelize);
+db.activity = require("./activity.model.js")(sequelize, Sequelize);
 
 db.user.hasOne(db.friendList, {
   foreignKey: "userId",
